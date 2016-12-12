ca1a29b18f9b51fd5545fc9be891872488b2080c
Tom Bertalan <tom@tombertalan.com>
2016-12-12 18:35:06 -0500

    Add a few projects for display.

--- bin/createSiteFromDirs.py
+++ bin/createSiteFromDirs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from os import listdir, mkdir
 from os.path import basename, join, isdir, dirname
+import codecs
 from shutil import copy
 from subprocess import check_output, CalledProcessError
 import markdown
@@ -32,14 +33,16 @@ for projectDir in projectDirs:
         configFile = utils.first([f for f in listdir(docFolder) if f == 'www.json'])
         if configFile is None:
             continue  # Skip this project if there's no www.json file.
+        print 'Loading', configFile, 'for project', basename(projectDir)
         config = json.load(open(join(docFolder, configFile), 'r'))
         if 'skip project' in config and config['skip project']:
             continue  # Skip this project if we're explicitly told to.
         print 'docFolder is', docFolder
         baseProjectName = config.get('project name', basename(projectDir))
-        hero = config.get('hero image', 'images/1px.png')
+        hero = config.get('hero image', None)
+        print 'Got hero image',  hero
         blurb = config.get('blurb', '')
-        description = config.get('description', 'see README.md')
+        description = config.get('description', None)
         if description == 'see README.md':
             
             for f in listdir(projectDir):
@@ -49,14 +52,15 @@ for projectDir in projectDirs:
                 #if len(basePath) >= 2 and basePath[-2:].lower() = 'md':
                     # Assume we've found a top-level markdown file.
                     print 'Found %s in directory %s.' % (basePath, projectDir)  
-                    description = open(path).read()
+                    description = codecs.open(path, mode="r", encoding="utf-8").read()
         
     else:
         continue  # Skip this project if there's not /*doc*/ subfolder.
-    
+
     # Prepare a folder to put the output in.
     destinationFileLocation = join(wwwDir, baseProjectName, 'index.html')
     linkDestination = join(baseProjectName, 'index.html')
+    print 'Link destination is', linkDestination
     try:
         mkdir(dirname(destinationFileLocation))
     except OSError:
@@ -64,29 +68,38 @@ for projectDir in projectDirs:
 
     # Copy hero image to output folder.        
     print 'hero is', hero
-    imgSrc = join(baseProjectName, basename(hero))
-    imgCpLoc = join(dirname(destinationFileLocation), basename(hero))
-    copy(join(projectDir, hero), imgCpLoc)
-    
-    # Try to construct a github link.
-    repo = None
-    try:
-        remotes = [l for l in check_output(["git", "remote", "-v"], cwd=projectDir).split('\n') if 'github' in l and 'bertalan' in l]
-        if len(remotes) > 0:
-            repo = 'http://github.com/' + remotes[0].split()[1][15:][:-4]
-    except CalledProcessError:
-        pass
+    if hero is None:
+        imgSrc = None
+    else:
+        imgSrc = join(baseProjectName, basename(hero))
+        fr = join(projectDir, hero)
+        imgCpLoc = join(dirname(destinationFileLocation), basename(hero))
+        to = imgCpLoc
+        print 'Copying from %s to %s.' % (fr, to)
+        copy(fr, to)
         
-    # Generate HTML for the page contents.
-    readmeHtml = markdown.markdown(description)
-    
-    # Generate and write the full project page. 
-    breadcrumbs = [utils.Tag('a', href='../index.html', tagText='Home'), utils.Tag('span', tagText=baseProjectName, parseTagText=False)]
-    pageHtml = article(
-                       #utils.Tag('a', href='../index.html', tagText='Tom Bertalan'),
-                       'Tom Bertalan',
-                       readmeHtml, breadcrumbs=breadcrumbs, sourceLink=repo, heading=False)
-    utils.writePage(pageHtml, destinationFileLocation)
+    if description is not None:    
+        # Try to construct a github link.
+        repo = None
+        try:
+            remotes = [l for l in check_output(["git", "remote", "-v"], cwd=projectDir).split('\n') if 'github' in l and 'bertalan' in l]
+            if len(remotes) > 0:
+                repo = 'http://github.com/' + remotes[0].split()[1][15:][:-4]
+        except CalledProcessError:
+            pass
+            
+        # Generate HTML for the page contents.
+        readmeHtml = markdown.markdown(description)
+        
+        # Generate and write the full project page. 
+        breadcrumbs = [utils.Tag('a', href='../index.html', tagText='Home'), utils.Tag('span', tagText=baseProjectName, parseTagText=False)]
+        pageHtml = article(
+                           #utils.Tag('a', href='../index.html', tagText='Tom Bertalan'),
+                           'Tom Bertalan',
+                           readmeHtml, breadcrumbs=breadcrumbs, sourceLink=repo, heading=False)
+        utils.writePage(pageHtml, destinationFileLocation)
+    else:
+        linkDestination = None
         
     # Generate a card for this project to put on the home page.
     cards.append(index.portfolioCard(baseProjectName, blurb, imgSrc=imgSrc, linkDest=linkDestination, imgAlt=blurb))
--- bin/index.py
+++ bin/index.py
@@ -7,23 +7,37 @@
 from page import Html
 from utils import child, Tag, Div, parseAnonymousHTML
 
-def portfolioCard(title, supportingText, linkDest='#', imgSrc='images/1px.png', imgAlt=''):
-    out = parseAnonymousHTML(
-    ''' <div class="mdl-cell mdl-card mdl-shadow--4dp portfolio-card">
-            <div class="mdl-card__media">
-                <img class="article-image" src="%s" border="0" alt="%s" width="100%%" />
-            </div>
-            <div class="mdl-card__title">
-                <h2 class="mdl-card__title-text">%s</h2>
-            </div>
-            <div class="mdl-card__supporting-text">
-                %s
-            </div>
-            <div class="mdl-card__actions mdl-card--border">
-                <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent" href="%s">Read more</a>
-            </div>
-         </div>''' % (imgSrc, imgAlt, title, supportingText, linkDest),
-        )[1][0]
+def portfolioCard(title, supportingText, linkDest=None, imgSrc=None, imgAlt=''):
+    print 'Generating card with linkDest=', linkDest
+    out = Div(cls='mdl-cell mdl-card mdl-shadow--4dp portfolio-card')
+    if imgSrc is not None:
+        out.append(Div(cls='mdl-card__media', toAppend=[
+            Tag('img', cls='article-image', src=imgSrc, border='0', alt=imgAlt, width='100%')
+                                                        ]))
+    out.append(Div(cls='mdl-card__title', toAppend=[
+        Tag('h2', cls='mdl-card__title-text', tagText=title)
+                                                    ]))
+    out.append(Div(cls='mdl-card__supporting-text', tagText=supportingText))
+    if linkDest is not None:
+        out.append(Div(cls='mdl-card__actions mdl-card--border', toAppend=[
+            Tag('a', cls='mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent', href=linkDest, tagText='Read more') 
+                                                                            ]))
+#     out = parseAnonymousHTML(
+#     ''' <div class="mdl-cell mdl-card mdl-shadow--4dp portfolio-card">
+#             <div class="mdl-card__media">
+#                 <img class="article-image" src="%s" border="0" alt="%s" width="100%%" />
+#             </div>
+#             <div class="mdl-card__title">
+#                 <h2 class="mdl-card__title-text">%s</h2>
+#             </div>
+#             <div class="mdl-card__supporting-text">
+#                 %s
+#             </div>
+#             <div class="mdl-card__actions mdl-card--border">
+#                 <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent" href="%s">Read more</a>
+#             </div>
+#          </div>''' % (imgSrc, imgAlt, title, supportingText, linkDest),
+#         )[1][0]
     return out
 
 
