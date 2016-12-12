4d35cccf5c822788e35f417d84b956f5fbd7d529
Tom Bertalan <tom@tombertalan.com>
2016-12-12 17:08:51 -0500

    Use a json file to describe displayable projects.
    E.g., for Gunnar:
    
        {
        "project name": "Gunnar",
        "blurb": "Gunnar is a robot.",
        "hero image": "doc/map.png"
        }

--- bin/createSiteFromDirs.py
+++ bin/createSiteFromDirs.py
@@ -4,6 +4,7 @@ from os.path import basename, join, isdir, dirname
 from shutil import copy
 from subprocess import check_output, CalledProcessError
 import markdown
+import json
 
 from standalonePage import article
 import utils
@@ -22,68 +23,71 @@ projectDirs = listdirs(baseDir)
 cards = []
 
 for projectDir in projectDirs:
-    baseProjectName = basename(projectDir)
-    contents = listdir(projectDir)
-    for path in contents:
-        path = join(projectDir, path)
-        basePath = basename(path)
-        if basePath == 'README.md':
-        #if len(basePath) >= 2 and basePath[-2:].lower() = 'md':
         
-            # Try to check for a github repo.
-            repo = None
-            try:
-                remotes = [l for l in check_output(["git", "remote", "-v"], cwd=projectDir).split('\n') if 'github' in l and 'bertalan' in l]
-                if len(remotes) > 0:
-                    repo = 'http://github.com/' + remotes[0].split()[1][15:][:-4]
-            except CalledProcessError:
-                pass
-                
-            # Prepare a folder to put the output in.
-            destination = join('.', baseProjectName, 'index.html')
-            try:
-                mkdir(dirname(destination))
-            except OSError:
-                pass
+    # Try to find a www.json file.
+    docFolder = utils.first([f for f in listdirs(projectDir) if 'doc' in basename(f).lower()])
+    imgSrc = 'images/1px.png'
+    if docFolder is not None:
+        configFile = utils.first([f for f in listdir(docFolder) if f == 'www.json'])
+        if configFile is None:
+            continue  # Skip this project if there's no www.json file.
+        config = json.load(open(join(docFolder, configFile), 'r'))
+        if 'skip project' in config and config['skip project']:
+            continue  # Skip this project if we're explicitly told to.
+        print 'docFolder is', docFolder
+        baseProjectName = config.get('project name', basename(projectDir))
+        hero = config.get('hero image', 'images/1px.png')
+        blurb = config.get('blurb', '')
+        description = config.get('description', 'see README.md')
+        if description == 'see README.md':
             
-            # Assume we've found a top-level markdown file.
-            print 'Found %s in directory %s.' % (basePath, projectDir)  
-            readmeHtml = markdown.markdown(open(path).read())
-            blurb = 'This is the blurb.'
-            
-            # Try to find a hero image.
-            docFolder = utils.first([f for f in listdirs(projectDir) if 'doc' in basename(f).lower()])
-            imgSrc = 'images/1px.png'
-            if docFolder is not None:
-                print 'docFolder is', docFolder
-                hero = utils.first([f for f in listdir(docFolder) if 'hero' in f])
-                if hero is not None:
-                    print 'hero is', hero
-                    imgSrc = join(dirname(destination), hero)
-                    copy(join(docFolder, hero), imgSrc)
-            breadcrumbs = [utils.Tag('a', href='../index.html', tagText='Home'), utils.Tag('span', tagText=baseProjectName, parseTagText=False)]
-
-            pageHtml = article(
-                               #utils.Tag('a', href='../index.html', tagText='Tom Bertalan'),
-                               'Tom Bertalan',
-                               readmeHtml, breadcrumbs=breadcrumbs, sourceLink=repo, heading=False)
-            
-            # Write the project page.
-            utils.writePage(pageHtml, destination)
-                
-            # Add the page to a list to make a home page.
-            cards.append(index.portfolioCard(baseProjectName, blurb, imgSrc=imgSrc, linkDest=destination, imgAlt=blurb))
+            for f in listdir(projectDir):
+                path = join(projectDir, f)
+                basePath = basename(path)
+                if basePath == 'README.md':
+                #if len(basePath) >= 2 and basePath[-2:].lower() = 'md':
+                    # Assume we've found a top-level markdown file.
+                    print 'Found %s in directory %s.' % (basePath, projectDir)  
+                    description = open(path).read()
+        
+    else:
+        continue  # Skip this project if there's not /*doc*/ subfolder.
     
+    # Prepare a folder to put the output in.
+    destination = join('.', baseProjectName, 'index.html')
+    try:
+        mkdir(dirname(destination))
+    except OSError:
+        pass
+
+    # Copy hero image to output folder.        
+    print 'hero is', hero
+    imgSrc = join(dirname(destination), basename(hero))
+    copy(join(projectDir, hero), imgSrc)
+    
+    # Try to construct a github link.
+    repo = None
+    try:
+        remotes = [l for l in check_output(["git", "remote", "-v"], cwd=projectDir).split('\n') if 'github' in l and 'bertalan' in l]
+        if len(remotes) > 0:
+            repo = 'http://github.com/' + remotes[0].split()[1][15:][:-4]
+    except CalledProcessError:
+        pass
+        
+    # Generate HTML for the page contents.
+    readmeHtml = markdown.markdown(description)
+    
+    # Generate and write the full project page. 
+    breadcrumbs = [utils.Tag('a', href='../index.html', tagText='Home'), utils.Tag('span', tagText=baseProjectName, parseTagText=False)]
+    pageHtml = article(
+                       #utils.Tag('a', href='../index.html', tagText='Tom Bertalan'),
+                       'Tom Bertalan',
+                       readmeHtml, breadcrumbs=breadcrumbs, sourceLink=repo, heading=False)
+    utils.writePage(pageHtml, destination)
+        
+    # Generate a card for this project to put on the home page.
+    cards.append(index.portfolioCard(baseProjectName, blurb, imgSrc=imgSrc, linkDest=destination, imgAlt=blurb))
+    
+# Generate, write, and display the home page. 
 homepageHtml = index.index(cards=cards)
 utils.displayHtml(homepageHtml, fname='./index.html')
-                
-    
-#     # Look for a doc[s]/www subdir.
-#     subdirs = listdirs(projectDir)
-#     for subdir in subdirs:
-#         if 'doc' in basename(subdir):
-#             docpath = subdir
-#             
-#             
-#             break
-#     # If we got here, we've either found and processed a doc subdir, or failed and should move on.
