2be0de011273a800707a0d6608a74038cad937f8
Tom Bertalan <tom@tombertalan.com>
2016-12-13 12:10:04 -0500

    Add some bio content and crop images smaller.

--- bin/index.py
+++ bin/index.py
@@ -12,36 +12,20 @@ def portfolioCard(title, supportingText, linkDest=None, imgSrc=None, imgAlt=''):
     out = Div(cls='mdl-cell mdl-card mdl-shadow--4dp portfolio-card mdl-cell--4-col')
     if imgSrc is not None:
         out.append(Div(cls='mdl-card__media', toAppend=[
-            Tag('img', cls='article-image', src=imgSrc, border='0', alt=imgAlt, width='100%')
+            Div(cls='card-img-heightcrop', style="background-image: url('%s');" % imgSrc),
                                                         ]))
     out.append(Div(cls='mdl-card__title', toAppend=[
         Tag('h2', cls='mdl-card__title-text', tagText=title)
                                                     ]))
-    out.append(Div(cls='mdl-card__supporting-text', tagText=supportingText))
+    out.append(Div(cls='mdl-card__supporting-text', style='padding-bottom: 70px;', tagText=supportingText))
     if linkDest is not None:
-        out.append(Div(cls='mdl-card__actions mdl-card--border', toAppend=[
+        out.append(Div(cls='mdl-card__actions mdl-card--border button-at-bottom', toAppend=[
             Tag('a', cls='mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent', href=linkDest, tagText='Read more') 
                                                                             ]))
-#     out = parseAnonymousHTML(
-#     ''' <div class="mdl-cell mdl-card mdl-shadow--4dp portfolio-card">
-#             <div class="mdl-card__media">
-#                 <img class="article-image" src="%s" border="0" alt="%s" width="100%%" />
-#             </div>
-#             <div class="mdl-card__title">
-#                 <h2 class="mdl-card__title-text">%s</h2>
-#             </div>
-#             <div class="mdl-card__supporting-text">
-#                 %s
-#             </div>
-#             <div class="mdl-card__actions mdl-card--border">
-#                 <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent" href="%s">Read more</a>
-#             </div>
-#          </div>''' % (imgSrc, imgAlt, title, supportingText, linkDest),
-#         )[1][0]
     return out
 
 
-def index(cards=[], DEBUG=False):
+def index(cards=[]):
     html = Html(lang='en')
     head = child(html, 'head')
     body = child(html, 'body')
@@ -55,22 +39,30 @@ def index(cards=[], DEBUG=False):
     mainContent = Tag('main', cls='mdl-layout__content')
     layout.append(mainContent)
     
-    if DEBUG:
-        grid = Div(cls='mdl-grid portfolio-max-width', style='border:1px solid brown;')
-        bioCell = Div(cls='mdl-cell mdl-cell--4-col', style='border:1px solid green;')
-        cardsCell = Div(cls='mdl-cell mdl-cell--8-col mdl-grid', style='border:1px solid black;')
-    else:
-        grid = Div(cls='mdl-grid portfolio-max-width')
-        bioCell = Div(cls='mdl-cell mdl-cell--4-col')
-        cardsCell = Div(cls='mdl-cell mdl-cell--8-col mdl-grid')
+    grid = Div(cls='mdl-grid portfolio-max-width')
+    bioCell = Div(cls='mdl-cell mdl-cell--4-col')
+    cardsCell = Div(cls='mdl-cell mdl-cell--8-col mdl-grid')
+    
 
     mainContent.append(grid)
     grid.append(bioCell)
     
     bioCell.append(Div(cls='portfolio-logo'))
-    bioCell.append(Tag('center', toAppend=[Tag('h1', cls='mdl-typography--display-3', style='color: #333;', tagText='Tom Bertalan')]))
+    bioCell.append(Tag('center', toAppend=[Tag('h1', cls='mdl-typography--display-3 h1-mainpage', tagText='Tom Bertalan')]))
+    bioCell.append(Div(cls='mdl-cell mdl-cell--12-col', toAppend=[
+                                                                  Tag('p', tagText='''
+    I'm a fifth-year grad student soon to receive my PhD from Princeton University's
+    department of Chemical and Biological Engineering, with a Graduate Certificate in Computational and Information Science.  
+                                                                  '''),
+                                                                  Tag('p', tagText='''
+    My research interests are in data mining and dimensionality reduction for high-dimensional dynamical systems,
+    with applications in computational neuroscience.
+    I also work on robotic design and perception projects.
+                                                                  '''),
+    ]))
     
     grid.append(cardsCell)
+    cardsCell.append(Div(cls='mdl-cell mdl-cell--12-col', toAppend=[Tag('h3', tagText='Projects')]))
     cardsCell.extend(cards)
     
     
